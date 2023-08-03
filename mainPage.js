b783c4b139ca3f7ba2c8efe952da3fed6e9a2633
EricChen0803 <erchen@ucsd.edu>
2023-08-03 14:19:20 -0700

    UPDATE

--- mainPage.js
+++ mainPage.js
@@ -22,9 +22,13 @@ function initialize(){
         buttonContainer.appendChild(row); // 将行添加到按钮容器中
         document.getElementById("start").textContent = "Restar Game";
     }
+    color = 0;
     array = new Array(15);
     for (let i = 0; i < 15; i++) {
         array[i] = new Array(15);
+        for(let j=0; j<15; j++){
+            array[i][j]="U";
+        }
       }
     }
 function placeButton(){
@@ -34,14 +38,123 @@ function placeButton(){
     if(color == 0){
         this.classList.add("Red");
         color = 1;
+        var elementId = this.id;
+        var parts = elementId.split(',');
+        var first = parseInt(parts[0]);
+        var second = parseInt(parts[1]);
+        array[first][second]="R";
         check_win();
     }
     else{
         this.classList.add("Blue");
+        var elementId = this.id;
+        var parts = elementId.split(',');
+        var first = parseInt(parts[0]);
+        var second = parseInt(parts[1]);
+        array[first][second]="B";
         color = 0;
         check_win();
     }
 }
 function check_win(){
+    for(let i=0; i<15; i++){
+        var longstr = array[i].join(''); // Convert array to a comma-separated string
 
+        var substringToCheckR = "RRRRR";
+        var substringToCheckB = "BBBBB";
+
+        if (longstr.includes(substringToCheckR)) {
+            alert("Red Win!!!");
+            return;
+        }
+        if (longstr.includes(substringToCheckB)) {
+            alert("Blue Win!!!");
+            return;
+        }
+    }
+    for(let i=0; i<15; i++){
+        var tparray = [];
+        for(let j=0; j<15; j++){
+            tparray.push(array[j][i]);
+        }
+        var longstr = tparray.join(''); // Convert array to a comma-separated string
+
+        var substringToCheckR = "RRRRR";
+        var substringToCheckB = "BBBBB";
+
+        if (longstr.includes(substringToCheckR)) {
+            alert("Red Win!!!");
+            return;
+        }
+        if (longstr.includes(substringToCheckB)) {
+            alert("Blue Win!!!");
+            return;
+        }
+    }
+    var countArray = new Array(30).fill(0);
+    var zxarray = new Array(30);
+    for (var i = 0; i < 30; i++) {
+        zxarray[i] = new Array(15);
+        for(let j=0; j<15; j++){
+            zxarray[i][j]="U";
+        }
+      }
+      for(let i=0; i<15; i++){
+        for(let j=0; j<15; j++){
+            if(i-j<0){
+                zxarray[30+(i-j)][countArray[30+(i-j)]]= array[i][j];
+                countArray[30+(i-j)]++;
+            }
+            else{
+                zxarray[(i-j)][countArray[(i-j)]]= array[i][j];
+                countArray[(i-j)]++;
+            }
+        }
+    }
+    console.log(zxarray);
+    for(let i=0; i<29; i++){
+        var longstr = zxarray[i].join(''); // Convert array to a comma-separated string
+
+        var substringToCheckR = "RRRRR";
+        var substringToCheckB = "BBBBB";
+
+        if (longstr.includes(substringToCheckR)) {
+            alert("Red Win!!!");
+            return;
+        }
+        if (longstr.includes(substringToCheckB)) {
+            alert("Blue Win!!!");
+            return;
+        }
+    }
+    countArray = new Array(30).fill(0);
+    zxarray = new Array(30);
+    for (var i = 0; i < 30; i++) {
+        zxarray[i] = new Array(15);
+        for(let j=0; j<15; j++){
+            zxarray[i][j]="U";
+        }
+      }
+      for(let i=0; i<15; i++){
+        for(let j=0; j<15; j++){
+            zxarray[(i+j)][countArray[(i+j)]]= array[i][j];
+            countArray[(i+j)]++;
+        }
+    }
+    console.log(zxarray);
+    for(let i=0; i<29; i++){
+        var longstr = zxarray[i].join(''); // Convert array to a comma-separated string
+
+        var substringToCheckR = "RRRRR";
+        var substringToCheckB = "BBBBB";
+
+        if (longstr.includes(substringToCheckR)) {
+            alert("Red Win!!!");
+            return;
+        }
+        if (longstr.includes(substringToCheckB)) {
+            alert("Blue Win!!!");
+            return;
+        }
+    }
 }
